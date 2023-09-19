44c43faff3d51faaa31164600ddc83750f08f95e
Kerberoge <sjoerdenjh@gmail.com>
2023-09-19 19:33:19 +0200

    finished body of main() function

--- main.c
+++ main.c
@@ -2,19 +2,74 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <time.h>
 
-void warnAndExit (void) {
+void warnAndExit (void)
+{
 	fprintf(stderr, "Error: invalid date: date must be of dd-mm-yyyy format\n");
 	exit(1);
 }
 
-int* parseInput (int* argc, char*** argv) {
+int isLeapYear (int year)
+{
+	if ( ( year % 100 == 0 && year % 400 == 0 ) || ( year % 100 != 0 && year % 4 == 0 ) )
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int numberOfDaysInMonth (int month, int year)
+{
+	int numberOfDays;
+	switch ( month )
+	{
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			numberOfDays = 31;
+			break;
+
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			numberOfDays = 30;
+			break;
+
+		case 2:
+			if ( isLeapYear(year) )
+			{
+				numberOfDays = 29;
+			}
+			else
+			{
+				numberOfDays = 28;
+			}
+			break;
+	}
+	
+	return numberOfDays;
+}
+
+int* parseInput (int* argc, char*** argv)
+{
 
 	// check if the user provided only one argument
-	if (*argc < 2) {
+	if (*argc < 2)
+	{
 		fprintf(stderr, "Error: no date supplied\n");
 		exit(1);
-	} else if (*argc > 2) {
+	}
+	else if (*argc > 2)
+	{
 		fprintf(stderr, "Error: too many arguments\n");
 		exit(1);
 	}
@@ -23,91 +78,131 @@ int* parseInput (int* argc, char*** argv) {
 	char date_strings[3][10] = { 0, 0, 0 };
 	int current_pos = 0;
 
-	for (int i = 0; i < strlen( (*argv)[1] ); i++) {
-		if ( current_pos > 2 ) {
+	for (int i = 0; i < strlen( (*argv)[1] ); i++)
+	{
+		if ( current_pos > 2 )
+		{
 			break;
-		} else if ( (*argv)[1][i] == '-' || (*argv)[1][i] == '/' || (*argv)[1][i] == '.' ) {
+		}
+		else if ( (*argv)[1][i] == '-' || (*argv)[1][i] == '/' || (*argv)[1][i] == '.' )
+		{
 			current_pos++;
-		} else if ( isdigit( (*argv)[1][i] ) ) {
+		}
+		else if ( isdigit( (*argv)[1][i] ) )
+		{
 			strncat(date_strings[current_pos], &(*argv)[1][i], 1);
-		} else {
+		}
+		else
+		{
 			warnAndExit();
 		}
 	}
 
-	if ( current_pos > 2 ) {
+	if ( current_pos > 2 )
+	{
 		warnAndExit();
 	}
 
-	for (int i = 0; i < 3; i++) {
-		if ( strlen(date_strings[i]) == 0 ) {
+	for (int i = 0; i < 3; i++)
+	{
+		if ( strlen(date_strings[i]) == 0 )
+		{
 			warnAndExit();
 		}
 	}
 
 	// convert the strings to numbers and place them into an array
 	int* date = calloc(3, sizeof(int));
-	for (int i = 0; i < 3; i++) {
+	for (int i = 0; i < 3; i++)
+	{
 		date[i] = atoi(date_strings[i]);
 	}
 
 	// check month
-	if ( date[1] > 12 || date[1] == 0 ) {
+	if ( date[1] > 12 || date[1] == 0 )
+	{
 		warnAndExit();
 	}
 
 	// check day
-	if ( date[0] == 0 ) {
+	if ( date[0] == 0 || date[0] > numberOfDaysInMonth(date[1], date[2]) )
+	{
 		warnAndExit();
-	}
-
-	switch ( date[1] ) {
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12:
-			if ( date[0] > 31 ) {
-				warnAndExit();
-			}
-			break;
-
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			if ( date[0] > 30 ) {
-				warnAndExit();
-			}
-			break;
-
-		case 2:
-			if ( date[0] > 29 ) {
-				warnAndExit();
-			}
-
-			if ( date[0] == 29 ) {
-				if ( date[2] % 100 == 0 && date[2] % 400 != 0 || date[2] % 4 != 0 ) {
-					warnAndExit();
-				}
-			}
-			break;
 	}
 
 	return date;
 }
 
-int main (int argc, char** argv) {
+int main (int argc, char** argv)
+{
 
-	int* array = parseInput(&argc, &argv);
+	int* user_date = parseInput(&argc, &argv);
 
-	printf("contents of for loop: ");
-	for (int i = 0; i < 3; i++) {
-		printf("%d ", array[i]);
+	// get current date
+	int current_date[3];
+	time_t t = time(NULL);
+	struct tm time = *localtime(&t);
+	current_date[0] = time.tm_mday;
+	current_date[1] = time.tm_mon + 1;
+	current_date[2] = time.tm_year + 1900;
+
+	// verify that provided date isn't in the future
+	if ( user_date[2] > current_date[2] ||
+		(user_date[1] > current_date[1] && user_date[2] == current_date[2]) ||
+		(user_date[0] > current_date[0] && user_date[1] == current_date[1] && user_date[2] == current_date[2]) )
+	{
+		fprintf(stderr, "Error: date is in the future\n");
+		exit(1);
 	}
-	printf("\n");
+
+	int amount_of_days = 0;
+
+	// count days till the same date in the year in current_date
+	for (int i = user_date[2]; i < current_date[2]; i++)
+	{
+		if ( isLeapYear(i) )
+		{
+			amount_of_days += 366;
+		}
+		else
+		{
+			amount_of_days += 365;
+		}
+	}
+
+	// add to or subtract from amount_of_days till the date in current_date is reached
+	if ( user_date[1] < current_date[1] )
+	{
+		amount_of_days += numberOfDaysInMonth(user_date[1], current_date[2]) - user_date[0];
+
+		for (int i = user_date[1] + 1; i < current_date[1]; i++)
+		{
+			amount_of_days += numberOfDaysInMonth(i, current_date[2]);
+		}
+
+		amount_of_days += current_date[0];
+	}
+	else if	( user_date[0] < current_date[0] && user_date[1] == current_date[1] )
+	{
+		amount_of_days += current_date[0] - user_date[0];
+	}
+	else if ( user_date[1] > current_date[1] )
+	{
+		amount_of_days -= user_date[0];
+
+		for (int i = user_date[1] - 1; i > current_date[1]; i--)
+		{
+			amount_of_days -= numberOfDaysInMonth(i, current_date[2]);
+		}
+
+		amount_of_days -= numberOfDaysInMonth(current_date[1], current_date[2]) - current_date[0];
+	}
+	else if ( user_date[0] > current_date[0] && user_date[1] == current_date[1] )
+	{
+		amount_of_days -= user_date[0] - current_date[0];
+	}
+
+	printf("Number of days that have passed since %02d-%02d-%04d: %d\n", user_date[0], user_date[1], user_date[2], amount_of_days);
 
 	return 0;
 }
